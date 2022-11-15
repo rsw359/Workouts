363d11bd933742f4c3a2be902e81d5efad4066a9
Roger Wells <rsw359@gmail.com>
2022-11-15 11:57:39 +0900

    brought in mongo

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
@@ -20,7 +21,15 @@ app.use((req, res, next) => {
 
 app.use("/api/workouts", workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000!");
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
